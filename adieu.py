f1f68cce6f41771ffabd0a3156fda8e913a7d83e
duanniten <duann_@hotmail.com>
2024-09-23 11:24:48 -0300

    implemetation all functions

---
+++ adieu.py
@@ -0,0 +1,31 @@
+def main():
+    #Get names
+    list_names = get_names()
+
+    #print names
+    text = text_names(list_names)
+
+    print(text)
+
+def get_names():
+    names = []
+    while True:
+        try: 
+            names.append(input("Name: "))
+        except EOFError:
+            return names
+
+def text_names(names : str):
+    leght  = len(names)
+    text ="\nAdieu, adieu, to "
+    if leght == 1: 
+        return text + names[0]
+    for i, name in enumerate(names):
+        if i == leght -1 :
+            text += f"and {name}"
+        else:
+            text += f"{name}, "
+    return text
+
+if __name__ == "__main__":
+    main()
